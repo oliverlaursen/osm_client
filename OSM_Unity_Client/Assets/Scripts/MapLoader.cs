f4f751e043d10a912b8c955efc3389a3cd563908
supersanta183 <klemmensen11@gmail.com>
2024-01-31 12:02:07 +0100

    mayve LatLonToPoint works :)

--- OSM_Unity_Client/Assets/Scripts/MapLoader.cs
+++ OSM_Unity_Client/Assets/Scripts/MapLoader.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GeoAPI.CoordinateSystems;
+using GeoAPI.CoordinateSystems.Transformations;
+using ProjNet.CoordinateSystems;
+using ProjNet.CoordinateSystems.Transformations;
 
 [System.Serializable]
 public class Node
@@ -8,6 +12,20 @@ public class Node
     public string id;
     public double lat;
     public double lon;
+}
+
+public class Point
+{
+    public string id;
+    public double x;
+    public double y;
+
+    public Point(string id, double x, double y)
+    {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
 }
 
 [System.Serializable]
@@ -26,7 +44,27 @@ public class PreprocessedOSM
 
 public class MapLoader : MonoBehaviour
 {
+    List<Node> nodeList;
+    public Point LatLonToPoint(Node node)
+    {
+        double lat = node.lat;
+        double lon = node.lon;
+
+        ICoordinateSystem sourceCS = GeographicCoordinateSystem.WGS84;
+        ICoordinateSystem targetCS = ProjectedCoordinateSystem.WebMercator;
+
+        // Create a coordinate transformation
+        ICoordinateTransformation transformation = new CoordinateTransformationFactory().CreateFromCoordinateSystems(sourceCS, targetCS);
+
+        // Transform the coordinates
+        double[] input = { longitude, latitude };
+        double[] output = transformation.MathTransform.Transform(input);
+
+        return new Point(node.id, output[0], output[1]);
+    }
     public TextAsset mapFile;
+
+
 
     
 }
