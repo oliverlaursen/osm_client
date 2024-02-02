ca6c4df9cec6b6545f090361f82639b05d438a00
Oliver Laursen <ral8310@gmail.com>
2024-02-02 14:29:18 +0100

    Added different lines and zoom/pan

---
+++ OSM_Unity_Client/Assets/CameraControl.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float zoomSpeed = 1000f;
+    public float minOrthoSize = 20f;
+    public float maxOrthoSize = 320f;
+    private float mouseSensitivity = 1f;
+    private Vector3 lastPosition;
+
+
+    void Update()
+    {
+        // Zoom
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        Camera.main.orthographicSize -= scroll * zoomSpeed;
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, minOrthoSize, maxOrthoSize);
+        mouseSensitivity = Camera.main.orthographicSize / maxOrthoSize;
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                lastPosition = Input.mousePosition;
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                var delta = Input.mousePosition - lastPosition;
+                transform.Translate(-delta.x * mouseSensitivity, -delta.y * mouseSensitivity, 0);
+                lastPosition = Input.mousePosition;
+            }
+        }
+    }
+}
