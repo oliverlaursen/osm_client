b55277693233f203880e80a223ee67daf099f561
supersanta183 <klemmensen11@gmail.com>
2024-04-24 13:07:19 +0200

    added first test

--- OSM_Unity_Client/Assets/Tests/editmode/biDijkstraTest.cs
+++ OSM_Unity_Client/Assets/Tests/editmode/biDijkstraTest.cs
@@ -9,7 +9,7 @@ public class NewTestScript
     Graph denmarkGraph;
     // A Test behaves as an ordinary method
     [Test]
-    public void NewTestScriptSimplePasses()
+    public void FindsCorrectDistance()
     {
         var denmarkGraph = MapController.DeserializeGraph("Assets/Maps/denmark.graph");
         long startNode = 280276691;
@@ -17,18 +17,8 @@ public class NewTestScript
         double expectedDistance = 324741;
 
         BiDijkstra biDijkstra = new BiDijkstra(denmarkGraph);
-        biDijkstra.FindShortestPath(startNode, endNode);
-
-        // Use the Assert class to test conditions
-    }
-
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator NewTestScriptWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        var pathResult = biDijkstra.FindShortestPath(startNode, endNode);
+        var distance = pathResult.distance;
+        Assert.AreEqual(expectedDistance, distance);
     }
 }
