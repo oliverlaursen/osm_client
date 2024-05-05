c1256ff021efa0f9dcf4db6067974cd5b9bf1cf1
Oliver Laursen <ral8310@gmail.com>
2024-05-05 21:52:05 +0200

    Renamed nodes in test

--- OSM_Unity_Client/Assets/Tests/editmode/biDijkstraTest.cs
+++ OSM_Unity_Client/Assets/Tests/editmode/biDijkstraTest.cs
@@ -65,80 +65,80 @@ public class BiDijkstraTests
 
     [Test]
     /*
-    Edges for 312310192:
-    Node: 312310202 Cost: 613
-    Node: 312310296 Cost: 691
-    Node: 312310777 Cost: 2524
-    Node: 1313060778 Cost: 1582
-    Bi-Edges for 312310192: 
-    Node: 312310202 Cost: 613
-    Node: 312310296 Cost: 691
-    Node: 1313060778 Cost: 1582
-    Node: 312310777 Cost: 2524
+    Edges for 1:
+    Node: 2 Cost: 613
+    Node: 9 Cost: 691
+    Node: 8 Cost: 2524
+    Node: 13 Cost: 1582
+    Bi-Edges for 1: 
+    Node: 2 Cost: 613
+    Node: 9 Cost: 691
+    Node: 13 Cost: 1582
+    Node: 8 Cost: 2524
 
-    Edges for 312310202:
-    Node: 311660177 Cost: 3501
-    Node: 312310192 Cost: 613
-    Node: 312310296 Cost: 574
-    Node: 2715370639 Cost: 7446
-    Bi-Edges for 312310202: 
-    Node: 312310192 Cost: 613
-    Node: 312310296 Cost: 574
-    Node: 311660177 Cost: 3501
-    Node: 2715370639 Cost: 7446
+    Edges for 2:
+    Node: 3 Cost: 3501
+    Node: 1 Cost: 613
+    Node: 9 Cost: 574
+    Node: 14 Cost: 7446
+    Bi-Edges for 2: 
+    Node: 1 Cost: 613
+    Node: 9 Cost: 574
+    Node: 3 Cost: 3501
+    Node: 14 Cost: 7446
 
-    Edges for 311660177:
-    Node: 312310202 Cost: 3501
-    Node: 564295783 Cost: 45
-    Bi-Edges for 311660177: 
-    Node: 312310202 Cost: 3501
-    Node: 311660134 Cost: 75
-    Node: 564295783 Cost: 45
+    Edges for 3:
+    Node: 2 Cost: 3501
+    Node: 5 Cost: 45
+    Bi-Edges for 3: 
+    Node: 2 Cost: 3501
+    Node: 4 Cost: 75
+    Node: 5 Cost: 45
 
-    Edges for 311660134:
-    Node: 311660177 Cost: 75
-    Node: 564295783 Cost: 53
-    Node: 1239728955 Cost: 1485
-    Bi-Edges for 311660134: 
-    Node: 1239728955 Cost: 1485
-    Node: 564295783 Cost: 53
+    Edges for 4:
+    Node: 3 Cost: 75
+    Node: 5 Cost: 53
+    Node: 15 Cost: 1485
+    Bi-Edges for 4: 
+    Node: 15 Cost: 1485
+    Node: 5 Cost: 53
 
-    Edges for 564295783:
-    Node: 311660134 Cost: 53
-    Node: 311660177 Cost: 45
-    Node: 312311692 Cost: 820
-    Bi-Edges for 564295783: 
-    Node: 311660134 Cost: 53
-    Node: 311660177 Cost: 45
-    Node: 312311692 Cost: 820
+    Edges for 5:
+    Node: 4 Cost: 53
+    Node: 3 Cost: 45
+    Node: 6 Cost: 820
+    Bi-Edges for 5: 
+    Node: 4 Cost: 53
+    Node: 3 Cost: 45
+    Node: 6 Cost: 820
 
-    Edges for 312311692:
-    Node: 312311600 Cost: 47
-    Node: 564295783 Cost: 820
-    Node: 2715370503 Cost: 60
-    Bi-Edges for 312311692: 
-    Node: 2715370503 Cost: 60
-    Node: 564295783 Cost: 820
-    Node: 312311600 Cost: 47
+    Edges for 6:
+    Node: 12 Cost: 47
+    Node: 5 Cost: 820
+    Node: 7 Cost: 60
+    Bi-Edges for 6: 
+    Node: 7 Cost: 60
+    Node: 5 Cost: 820
+    Node: 12 Cost: 47
 
-    Edges for 2715370503:
-    Node: 312310777 Cost: 874
-    Node: 312311600 Cost: 83
-    Node: 312311692 Cost: 60
-    Bi-Edges for 2715370503: 
-    Node: 312311692 Cost: 60
-    Node: 312310777 Cost: 874
+    Edges for 7:
+    Node: 8 Cost: 874
+    Node: 12 Cost: 83
+    Node: 6 Cost: 60
+    Bi-Edges for 7: 
+    Node: 6 Cost: 60
+    Node: 8 Cost: 874
 
-    Edges for 312310777:
-    Node: 312310192 Cost: 2524
-    Node: 312310920 Cost: 159
-    Node: 312311676 Cost: 612
-    Node: 2715370503 Cost: 874
-    Bi-Edges for 312310777: 
-    Node: 312310192 Cost: 2524
-    Node: 2715370503 Cost: 874
-    Node: 312310920 Cost: 159
-    Node: 312311676 Cost: 612
+    Edges for 8:
+    Node: 1 Cost: 2524
+    Node: 10 Cost: 159
+    Node: 11 Cost: 612
+    Node: 7 Cost: 874
+    Bi-Edges for 8: 
+    Node: 1 Cost: 2524
+    Node: 7 Cost: 874
+    Node: 10 Cost: 159
+    Node: 11 Cost: 612
 
 
 
@@ -150,64 +150,64 @@ public class BiDijkstraTests
     */
     public void isolatedTest(){
         Dictionary<long, Edge[]> graph = new();
-        graph[312310192] = new Edge[]{new Edge{node = 312310202, cost = 613}, new Edge{node = 312310296, cost = 691}, new Edge{node = 312310777, cost = 2524}, new Edge{node = 1313060778, cost = 1582}};
-        graph[312310202] = new Edge[]{new Edge{node = 311660177, cost = 3501}, new Edge{node = 312310192, cost = 613}, new Edge{node = 312310296, cost = 574}, new Edge{node = 2715370639, cost = 7446}};
-        graph[311660177] = new Edge[]{new Edge{node = 312310202, cost = 3501}, new Edge{node = 564295783, cost = 45}};
-        graph[311660134] = new Edge[]{new Edge{node = 311660177, cost = 75}, new Edge{node = 564295783, cost = 53}, new Edge{node = 1239728955, cost = 1485}};
-        graph[564295783] = new Edge[]{new Edge{node = 311660134, cost = 53}, new Edge{node = 311660177, cost = 45}, new Edge{node = 312311692, cost = 820}};
-        graph[312311692] = new Edge[]{new Edge{node = 312311600, cost = 47}, new Edge{node = 564295783, cost = 820}, new Edge{node = 2715370503, cost = 60}};
-        graph[2715370503] = new Edge[]{new Edge{node = 312310777, cost = 874}, new Edge{node = 312311600, cost = 83}, new Edge{node = 312311692, cost = 60}};
-        graph[312310777] = new Edge[]{new Edge{node = 312310192, cost = 2524}, new Edge{node = 312310920, cost = 159}, new Edge{node = 312311676, cost = 612}, new Edge{node = 2715370503, cost = 874}};
-        graph[312310296] = new Edge[]{};
-        graph[312310920] = new Edge[]{};
-        graph[312311676] = new Edge[]{};
-        graph[312311600] = new Edge[]{};
-        graph[1313060778] = new Edge[]{};
-        graph[2715370639] = new Edge[]{};
-        graph[1239728955] = new Edge[]{};
+        graph[1] = new Edge[]{new Edge{node = 2, cost = 613}, new Edge{node = 9, cost = 691}, new Edge{node = 8, cost = 2524}, new Edge{node = 13, cost = 1582}};
+        graph[2] = new Edge[]{new Edge{node = 3, cost = 3501}, new Edge{node = 1, cost = 613}, new Edge{node = 9, cost = 574}, new Edge{node = 14, cost = 7446}};
+        graph[3] = new Edge[]{new Edge{node = 2, cost = 3501}, new Edge{node = 5, cost = 45}};
+        graph[4] = new Edge[]{new Edge{node = 3, cost = 75}, new Edge{node = 5, cost = 53}, new Edge{node = 15, cost = 1485}};
+        graph[5] = new Edge[]{new Edge{node = 4, cost = 53}, new Edge{node = 3, cost = 45}, new Edge{node = 6, cost = 820}};
+        graph[6] = new Edge[]{new Edge{node = 12, cost = 47}, new Edge{node = 5, cost = 820}, new Edge{node = 7, cost = 60}};
+        graph[7] = new Edge[]{new Edge{node = 8, cost = 874}, new Edge{node = 12, cost = 83}, new Edge{node = 6, cost = 60}};
+        graph[8] = new Edge[]{new Edge{node = 1, cost = 2524}, new Edge{node = 10, cost = 159}, new Edge{node = 11, cost = 612}, new Edge{node = 7, cost = 874}};
+        graph[9] = new Edge[]{};
+        graph[10] = new Edge[]{};
+        graph[11] = new Edge[]{};
+        graph[12] = new Edge[]{};
+        graph[13] = new Edge[]{};
+        graph[14] = new Edge[]{};
+        graph[15] = new Edge[]{};
 
 
         Dictionary<long, Edge[]> bi_graph = new();
-        bi_graph[312310192] = new Edge[]{new Edge{node = 312310202, cost = 613}, new Edge{node = 312310296, cost = 691}, new Edge{node = 1313060778, cost = 1582}, new Edge{node = 312310777, cost = 2524}};
-        bi_graph[312310202] = new Edge[]{new Edge{node = 312310192, cost = 613}, new Edge{node = 312310296, cost = 574}, new Edge{node = 311660177, cost = 3501}, new Edge{node = 2715370639, cost = 7446}};
-        bi_graph[311660177] = new Edge[]{new Edge{node = 312310202, cost = 3501}, new Edge{node = 311660134, cost = 75}, new Edge{node = 564295783, cost = 45}};
-        bi_graph[311660134] = new Edge[]{new Edge{node = 1239728955, cost = 1485}, new Edge{node = 564295783, cost = 53}};
-        bi_graph[564295783] = new Edge[]{new Edge{node = 311660134, cost = 53}, new Edge{node = 311660177, cost = 45}, new Edge{node = 312311692, cost = 820}};
-        bi_graph[312311692] = new Edge[]{new Edge{node = 312311600, cost = 47}, new Edge{node = 564295783, cost = 820}, new Edge{node = 2715370503, cost = 60}};
-        bi_graph[2715370503] = new Edge[]{new Edge{node = 312311692, cost = 60}, new Edge{node = 312310777, cost = 874}};
-        bi_graph[312310777] = new Edge[]{new Edge{node = 312310192, cost = 2524}, new Edge{node = 2715370503, cost = 874}, new Edge{node = 312310920, cost = 159}, new Edge{node = 312311676, cost = 612}};
-        bi_graph[312310296] = new Edge[]{};
-        bi_graph[312310920] = new Edge[]{};
-        bi_graph[312311676] = new Edge[]{};
-        bi_graph[312311600] = new Edge[]{};
-        bi_graph[1313060778] = new Edge[]{};
-        bi_graph[2715370639] = new Edge[]{};
-        bi_graph[1239728955] = new Edge[]{};
+        bi_graph[1] = new Edge[]{new Edge{node = 2, cost = 613}, new Edge{node = 9, cost = 691}, new Edge{node = 13, cost = 1582}, new Edge{node = 8, cost = 2524}};
+        bi_graph[2] = new Edge[]{new Edge{node = 1, cost = 613}, new Edge{node = 9, cost = 574}, new Edge{node = 3, cost = 3501}, new Edge{node = 14, cost = 7446}};
+        bi_graph[3] = new Edge[]{new Edge{node = 2, cost = 3501}, new Edge{node = 4, cost = 75}, new Edge{node = 5, cost = 45}};
+        bi_graph[4] = new Edge[]{new Edge{node = 15, cost = 1485}, new Edge{node = 5, cost = 53}};
+        bi_graph[5] = new Edge[]{new Edge{node = 4, cost = 53}, new Edge{node = 3, cost = 45}, new Edge{node = 6, cost = 820}};
+        bi_graph[6] = new Edge[]{new Edge{node = 12, cost = 47}, new Edge{node = 5, cost = 820}, new Edge{node = 7, cost = 60}};
+        bi_graph[7] = new Edge[]{new Edge{node = 6, cost = 60}, new Edge{node = 8, cost = 874}};
+        bi_graph[8] = new Edge[]{new Edge{node = 1, cost = 2524}, new Edge{node = 7, cost = 874}, new Edge{node = 10, cost = 159}, new Edge{node = 11, cost = 612}};
+        bi_graph[9] = new Edge[]{};
+        bi_graph[10] = new Edge[]{};
+        bi_graph[11] = new Edge[]{};
+        bi_graph[12] = new Edge[]{};
+        bi_graph[13] = new Edge[]{};
+        bi_graph[14] = new Edge[]{};
+        bi_graph[15] = new Edge[]{};
 
         Dictionary<long, (float[], double[])> nodes = new();
-        nodes[312310192] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
-        nodes[312310202] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
-        nodes[311660177] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
-        nodes[311660134] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
-        nodes[564295783] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
-        nodes[312311692] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
-        nodes[2715370503] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
-        nodes[312310777] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
-        nodes[312310296] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
-        nodes[312310920] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
-        nodes[312311676] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
-        nodes[312311600] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
-        nodes[1313060778] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
-        nodes[2715370639] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
-        nodes[1239728955] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
+        nodes[1] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
+        nodes[2] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
+        nodes[3] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
+        nodes[4] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
+        nodes[5] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
+        nodes[6] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
+        nodes[7] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
+        nodes[8] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
+        nodes[9] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
+        nodes[10] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
+        nodes[11] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
+        nodes[12] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
+        nodes[13] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
+        nodes[14] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
+        nodes[15] = (new float[]{12.0f, 55.0f}, new double[]{0.0});
         List<Landmark> landmarks = new();
 
         Graph smallGraph = new Graph{nodes = nodes, graph = graph, bi_graph = bi_graph, landmarks = landmarks};
         BiDijkstra biDijkstra = new BiDijkstra(smallGraph);
         Dijkstra dijkstra = new Dijkstra(smallGraph);
-        var dijkstraResult = dijkstra.FindShortestPath(312310192, 311660134);
-        var bidijkstraResult = biDijkstra.FindShortestPath(312310192, 311660134);
-        Assert.AreEqual(dijkstra.distances, bidijkstraResult.distance);
+        var dijkstraResult = dijkstra.FindShortestPath(1, 4);
+        var bidijkstraResult = biDijkstra.FindShortestPath(1, 4);
+        Assert.AreEqual(dijkstraResult.distance, bidijkstraResult.distance);
     }
 
 }
